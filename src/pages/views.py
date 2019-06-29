13cf8b2871a9fadd823d2bb3dcd7b93d96a795f7
Adam Hosman <github@adamsinbox.com>
2019-06-29 12:40:38 +0200

    Add context in contact_view

--- src/pages/views.py
+++ src/pages/views.py
@@ -7,7 +7,13 @@ def home_view(request, *args, **kwargs):
 
 
 def contact_view(request, *args, **kwargs):
-    return render(request, 'contact.html', {})
+    context: dict = {
+        'phone': '+12 345 6789',
+        'email': 'mail@gmail.com',
+        'address': '1 Best Street',
+        'stuff': [123, 456, 789]
+    }
+    return render(request, 'contact.html', context)
 
 
 def injected_view(request, *args, **kwargs):
